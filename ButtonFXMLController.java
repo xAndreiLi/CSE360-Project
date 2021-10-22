3c994026a4cf1dbc83b5a97df189f7348fca72c8
xAndreiLi <andreili@asu.edu>
2021-10-22 13:33:30 -0700

    Added main.java, Controller, and edited fxml files

---
+++ ButtonFXMLController.java
@@ -0,0 +1,38 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+public class ButtonFXMLController implements Initializable{
+
+	@FXML
+	private Button createAccountReq;
+
+    
+    @FXML 
+	protected void handleCreateAccountButtonAction(ActionEvent e){
+ 		Stage stage;
+		Parent root;
+		System.out.println("Pressed");
+		stage = (Stage) createAccountReq.getScene().getWindow();
+		root = FXMLLoader.load(getClass().getResource("FXML/createAccount.fxml"));
+
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();		
+    }
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb){
+		
+	}
+
+}
---
+++ main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.io.File;
+
+public class main extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/loginScreen.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
