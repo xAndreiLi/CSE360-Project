cbe2246b4f50865a6c4fb5277d69101cbae0c23e
Randy Pham <randy.default@gmail.com>
2021-11-05 11:07:25 -0700

    started initial work on doctor class

--- Doctor.java
+++ Doctor.java
@@ -1,3 +1,34 @@
+/**
+ * Doctor class is responsible for examining Patient objects
+ * 
+ * @author Randy Pham
+ */
 public class Doctor {
-    // worked on randy
+
+    public Patient currentPatient;
+
+    private MessageHandler messageHandler;
+
+    public void setPatient(Patient patient) {
+        currentPatient = patient;
+    }
+
+    public void examinePatient() {
+        // examines currentPatient here
+
+        updatePatientHistory();
+        prescribeMedication();
+    }
+
+    public void messagePatient(Patient patient) {
+        // uses messageHandler to message Patient
+    }
+
+    private void updatePatientHistory() {
+
+    }
+
+    private void prescribeMedication() {
+
+    }
 }
