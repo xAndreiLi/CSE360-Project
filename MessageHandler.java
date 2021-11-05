375fb827b5f082f9ee627381685c5038491383db
Randy Pham <randy.default@gmail.com>
2021-11-05 11:56:31 -0700

    create new message function completed?

--- MessageHandler.java
+++ MessageHandler.java
@@ -1,7 +1,5 @@
 import java.io.*;
 
-import javax.print.Doc;
-
 /**
  * Message Handler class handles all message-related functions. Uses I/O file
  * system to handle messages.
@@ -18,7 +16,21 @@ public class MessageHandler {
 
     }
 
-    public void createNewMessage(Doctor doctor, Patient patient) {
-        message = new File("message.txt");
+    public void createNewMessage(Doctor doctor, Patient patient) throws IOException {
+        // create a file
+        String fileName = "message.txt";
+        int fileNum = 0;
+
+        message = new File(fileName);
+        // this checks if the file name already exists
+        if (message.exists() && !message.isDirectory()) {
+            // if the file name already exists, simply add a number at the end of it
+            while (message.exists()) {
+                fileNum++;
+                message = new File("messages" + fileNum + ".txt");
+            }
+        }
+        // create new file with that file name
+        message.createNewFile();
     }
 }
