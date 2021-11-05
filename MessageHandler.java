b7d532ed5b287455d7c1276ed88dd003da20f8b9
Randy Pham <randy.default@gmail.com>
2021-11-05 11:36:02 -0700

    started message handler

--- MessageHandler.java
+++ MessageHandler.java
@@ -1,3 +1,24 @@
+import java.io.*;
+
+import javax.print.Doc;
+
+/**
+ * Message Handler class handles all message-related functions. Uses I/O file
+ * system to handle messages.
+ * 
+ * @author Randy Pham
+ */
 public class MessageHandler {
-	
+    File message;
+    Doctor doctor;
+    Patient patient;
+
+    // default constructor
+    public MessageHandler() {
+
+    }
+
+    public void createNewMessage(Doctor doctor, Patient patient) {
+        message = new File("message.txt");
+    }
 }
