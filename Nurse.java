7681732075da37750d8fcb5848a76e80ef75a7d5
Long Ly <lylong334@gmail.com>
2021-11-05 12:01:52 -0700

    testing

--- Nurse.java
+++ Nurse.java
@@ -1,4 +1,20 @@
 public class Nurse {
-	//work done by long
-    
+	//work done by Long Ly
+    //Nurse responsibities
+    /*
+    Current Patient
+    List of all doctors
+    View Patient List
+    Records Patient Measurements
+    Appoint Doctor to Patient
+    Add new patient to patient list
+    Remove patient from patient list
+    */
+
+    //Nurse Constructor
+    public Nurse(){
+
+    }//end of Nurse Constructor
+
+   
 }
