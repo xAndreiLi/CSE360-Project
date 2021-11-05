a86d7d74d6263e1c5448ca12010d7e611729851e
Hector Durazo <hectordurazo1@gmail.com>
2021-11-05 11:39:27 -0700

    added nurseSelectPatientPage.fxml

---
+++ FXML/NurseSelectPatientPageController.java
@@ -0,0 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button PatientListButton;
+
+    @FXML
+    private Button editVitalsButton;
+
+    @FXML
+    private Text selectedPatient;
+
+}
