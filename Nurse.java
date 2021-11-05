14685ca74f8b63dd41f3e1ddae7e295d06722fff
Long Ly <lylong334@gmail.com>
2021-11-05 12:45:39 -0700

    lit

--- Nurse.java
+++ Nurse.java
@@ -1,4 +1,3 @@
-import Patient;
 
 public class Nurse {
 	//work done by Long Ly
@@ -17,16 +16,38 @@ public class Nurse {
     //WIP
     private Patient patient;
     private Doctor[] doctors;
-    private PatientList PatientList;
+    private PatientList patientList;
     private MessageHandler messagehandler;
 
     public Nurse(){
+        this.patient = NULL;
+        this.doctors = NULL;
+        this.patientList = NULL;
         
     }//end of Nurse Constructor
 
-    public void updatePatientVitals(float weight, float height, float bodytemp, float bloodPres){
-        updateVitals(weight, height, bodytemp, bloodPres);
-    }
 
+    //updating Patient Vitals from Patient class
+    public void updatePatientVitals(float weight, String height, float bodytemp, float bloodPres){
+        this.patient.updateVitals(weight, height, bodytemp, bloodPres);
+    }//end of updatePaitentVitals
+
+    public void recordPatientMeasurements(Patient patient){ 
+        //may be replaced by updatePatientVitals
+    }//end of recordPatientMeasurements
+
+    private void viewPatientList(){
+        //TO BE WORKED ON
+    }//end of viewPatientList
+
+    private void addNewPatientToList(Patient patient){
+        //ADDS PATIENT TO PATIENTLIST
+
+    }//end of addNewPatientToList
+
+    private void removePatientFromList(Patient patient){
+        //REMOVE PATIENT FROM PATIENTLIST
+
+    }//removePatientFromList
    
-}
+}//end of nurse class
