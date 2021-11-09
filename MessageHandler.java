4ba0f760b02b06388d539e7a2a6cf825cf0fe1f7
Randy Pham <randy.default@gmail.com>
2021-11-09 14:25:30 -0700

    create new message now creates a new file and writes the message to that file

--- MessageHandler.java
+++ MessageHandler.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
  * @author Randy Pham
  */
 public class MessageHandler {
-    File message;
     Doctor doctor;
     Patient patient;
 
@@ -17,24 +16,37 @@ public class MessageHandler {
 
     }
 
-    // creates a new message
+    // creates a new file and writes the message between the doctor and the patient
+    // to it
     // TODO: need to figure out how to link the message to the doctor and patient
-    public void createNewMessage(Doctor doctor, Patient patient) throws IOException {
-        // create a file
+    public void createNewMessage(Doctor doctor, Patient patient, String message) throws IOException {
+        File messageFile;
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
         String fileName = "message.txt";
         int fileNum = 0;
 
-        message = new File(fileName);
+        messageFile = new File(fileName);
         // this checks if the file name already exists
-        if (message.exists() && !message.isDirectory()) {
+        if (messageFile.exists() && !messageFile.isDirectory()) {
             // if the file name already exists, simply add a number at the end of it
-            while (message.exists()) {
+            while (messageFile.exists()) {
                 fileNum++;
-                message = new File("messages" + fileNum + ".txt");
+                messageFile = new File("message" + fileNum + ".txt");
             }
         }
         // create new file with that file name
-        message.createNewFile();
+        messageFile.createNewFile();
+
+        // writing the file here
+        fileWriter = new FileWriter(messageFile);
+        bufferedWriter = new BufferedWriter(fileWriter);
+        // write the message to the file
+        bufferedWriter.write(message);
+
+        // close writer to prevent leak
+        bufferedWriter.close();
+        System.out.println("File writing done");
     }
 
     /**
@@ -45,17 +57,17 @@ public class MessageHandler {
      * @throws FileNotFoundException exception
      */
     public void readMessage(String filename) throws FileNotFoundException {
-        File message;
+        File messageFile;
         Scanner reader;
         String data;
 
-        message = findMessage(filename);
+        messageFile = findMessage(filename);
 
-        if (message == null) {
+        if (messageFile == null) {
             // UH OH THERE IS NO EXISTING MESSAGE
-            System.out.println("Cannot find message: " + message);
+            System.out.println("Cannot find message: " + messageFile);
         } else {
-            reader = new Scanner(message);
+            reader = new Scanner(messageFile);
 
             while (reader.hasNextLine()) {
                 data = reader.nextLine();
