d49aa8069a0fa1acee5836c62c6960c1dbc8284f
Randy Pham <randy.default@gmail.com>
2021-11-11 11:14:24 -0700

    changed the patient history to a Queue

--- Patient.java
+++ Patient.java
@@ -3,21 +3,24 @@
 // 11-05-2021
 // Still waiting on the Message Handler to set up operations. 
 // Still need to work on the patient History
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Patient {
 
-    //Patient Attributes
-    float weight, bodyTemp, bloodPres; //vitals taken by Nurse
-    String patientEmail, patientFName, patientLName, patientNumber, height,//Patient info and reason for visit
-        emerFName, emerLName, emerEmail, emergenNumber, medication; //Emergency Contact Info
+    // Patient Attributes
+    float weight, bodyTemp, bloodPres; // vitals taken by Nurse
+    String patientEmail, patientFName, patientLName, patientNumber, height, // Patient info and reason for visit
+            emerFName, emerLName, emerEmail, emergenNumber, medication; // Emergency Contact Info
     Doctor currentDoctor;
     MessageHandler messageHandler;
-    String[] patientHistory, dateOfBirth, visitReason;
+    String[] dateOfBirth, visitReason;
+    public Queue<String> patientHistory = new LinkedList<>();
 
-    //Patient Operations
+    // Patient Operations
 
-    public Patient(){
-        //Empty Patient 
+    public Patient() {
+        // Empty Patient
         this.patientFName = "John";
         this.patientLName = "Doe";
         this.dateOfBirth = null;
@@ -29,14 +32,16 @@ public class Patient {
         this.emerEmail = "janedoe@doe.com";
         this.emergenNumber = "000-000-0000";
         this.medication = "";
-        //Setting default vitals
+        // Setting default vitals
         this.weight = 0;
         this.height = "0 ' 0";
         this.bodyTemp = 0;
         this.bloodPres = 0;
 
     }
-    public Patient(String fName, String lName, String[] birthday, String[] visitReason, String email, String phoneNum, String emerfName, String emerlName, String emerEmail, String emergenNumber){
+
+    public Patient(String fName, String lName, String[] birthday, String[] visitReason, String email, String phoneNum,
+            String emerfName, String emerlName, String emerEmail, String emergenNumber) {
         this.patientFName = fName;
         this.patientLName = lName;
         this.dateOfBirth = birthday;
@@ -47,105 +52,110 @@ public class Patient {
         this.emerLName = emerlName;
         this.emerEmail = emerEmail;
         this.emergenNumber = emergenNumber;
-        //Setting default vitals
+        // Setting default vitals
         this.weight = 0;
         this.height = "0 ' 0";
         this.bodyTemp = 0;
         this.bloodPres = 0;
 
     }
-    //This function will be used by the Patients to mesage their doctors
-    //It'll take in a Doctor as the receiptient of the message, and the message itself
-    void messageDoctor(Doctor dr, String messString){
-        //To be worked on!!
+
+    // This function will be used by the Patients to mesage their doctors
+    // It'll take in a Doctor as the receiptient of the message, and the message
+    // itself
+    void messageDoctor(Doctor dr, String messString) {
+        // To be worked on!!
     }
 
-    //This message will be used to update a patient contact information.
-    //It will return a boolean if information is updated
-    boolean changeContactInformation(String email, String phoneNum){
+    // This message will be used to update a patient contact information.
+    // It will return a boolean if information is updated
+    boolean changeContactInformation(String email, String phoneNum) {
         boolean success = false;
-        if (email != ""){
+        if (email != "") {
             this.patientEmail = email;
             success = true;
-        } if (phoneNum != ""){
+        }
+        if (phoneNum != "") {
             this.patientNumber = phoneNum;
             success = true;
         }
         return success;
     }
 
-    String[] getPatientHistory(){
+    Queue<String> getPatientHistory() {
         return this.patientHistory;
     }
 
-    void updateVitals(float weight, String height, float bodyTemp, float bloodPres){
+    void updateVitals(float weight, String height, float bodyTemp, float bloodPres) {
         this.weight = weight;
         this.height = height;
         this.bodyTemp = bodyTemp;
         this.bloodPres = bloodPres;
     }
 
-    void assignDoctor(Doctor doctor){
+    void assignDoctor(Doctor doctor) {
         this.currentDoctor = doctor;
     }
 
-    void assignMedication(String medication){
+    void assignMedication(String medication) {
         this.medication = medication;
     }
 
-    //Getters for the View Own Patient Info
-    float getWeight(){
+    // Getters for the View Own Patient Info
+    float getWeight() {
         return this.weight;
     }
 
-    float getBodyTemp(){
+    float getBodyTemp() {
         return this.bodyTemp;
     }
 
-    float getbloodPres(){
+    float getbloodPres() {
         return this.bloodPres;
     }
 
-    String getPatientEmail(){
+    String getPatientEmail() {
         return this.patientEmail;
     }
 
-    String getPatientFirstName(){
+    String getPatientFirstName() {
         return this.patientFName;
     }
 
-    String getPatientLastName(){
+    String getPatientLastName() {
         return this.patientLName;
     }
-    String getPatientFullName(){
+
+    String getPatientFullName() {
         return this.patientFName + " " + this.patientLName;
     }
 
-    String getPhoneNumber(){
+    String getPhoneNumber() {
         return this.patientNumber;
     }
 
-    String getHeight(){
+    String getHeight() {
         return this.height;
     }
 
-    String getEmergencyFirstName(){
+    String getEmergencyFirstName() {
         return this.emerFName;
     }
 
-    String getEmergencyLastName(){
+    String getEmergencyLastName() {
         return this.emerLName;
     }
-    String getEmergencyFullName(){
+
+    String getEmergencyFullName() {
         return this.emerFName + " " + this.emerLName;
     }
 
-    String getEmergencyEmail(){
+    String getEmergencyEmail() {
         return this.emerEmail;
     }
-    String getEmergencyPhoneNumb(){
+
+    String getEmergencyPhoneNumb() {
         return this.emergenNumber;
     }
 
-    
 }
