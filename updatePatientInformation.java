c267dc6506fc4275be5bdbfb4942e441c7d33f6d
Hector Durazo <hectordurazo1@gmail.com>
2021-11-17 21:01:20 -0700

    finished update information screen

--- updatePatientInformation.java
+++ updatePatientInformation.java
@@ -24,25 +24,42 @@ public class updatePatientInformation extends Controller{
     @FXML
     private Text errorMessage;
 
+
     @Override
     public void initData() {
         Patient currentPatient = (Patient)super.currentUser;
         emerContFirstName.setText(currentPatient.getEmergencyFirstName());
-        EmerContLastName.setText(currentPatient.getEmergencyLastName()
-        EmerContEmail.setText()        
-    }
-    @FXML
-    private void accountUpdateconfirmButton(Action event) throws IOException{
-        Patient currentPatient = (Patient)super.currentUser;
-
-        currentPatient.setUsername(username.getText());
-        currentPatient.setPatientFirstName(firstName.getText());
-        currentPatient.set
-
-    }
-    @FXML
-    void handleBackButton(ActionEvent event) {
+        EmerContLastName.setText(currentPatient.getEmergencyLastName());
+        EmerContEmail.setText(currentPatient.getEmergencyEmail());
+        EmerContPhone.setText(currentPatient.getEmergencyPhoneNumb());
         
+        firstName.setText(currentPatient.getPatientFirstName());
+        lastName.setText(currentPatient.getPatientLastName());
+        email.setText(currentPatient.getPatientEmail());
+        password.setText(currentPatient.getPassword());
+        username.setText(currentPatient.getUsername());
+        pharmacyPref.setText(currentPatient.getPharmacy());
+        phoneNumber.setText(currentPatient.getPhoneNumber());
     }
 
+    @FXML
+    private void accountUpdateconfirmButton(ActionEvent event) throws IOException{
+        Patient cp = (Patient)super.currentUser;
+
+        cp.setPatientFirstName(firstName.getText());
+        cp.setPatientLastName(lastName.getText());
+        cp.setPatientEmail(email.getText());
+        cp.setPassword(password.getText());
+        cp.setUsername(username.getText());
+        cp.setPharmacy(pharmacyPref.getText());
+        cp.setPatientPhoneNumber(phoneNumber.getText());
+
+        cp.setEmergencyContact(emerContFirstName.getText(), EmerContLastName.getText(), EmerContEmail.getText(), EmerContPhone.getText());
+
+        errorMessage.setText("Updated");
+    }
+    @FXML
+    void handleBackButton(ActionEvent event) throws IOException{
+        backButton("FXML/PatientMainMenu.fxml", accountUpdateBACKButton);
+    }
 }
